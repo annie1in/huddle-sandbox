9d71e5bd16fc0a33a5807545fdadc74ac9047f95
annie1in <annieyoen@gmail.com>
2026-03-11 15:19:43 -0700

    edited mock data

--- src/data/studyGroups.js
+++ src/data/studyGroups.js
@@ -1,5 +1,26 @@
 export const studyGroups = [
   {
-    
+    id: 1,
+    course: "CS61A",
+    pace: "Fast",
+    noiseLevel: "Moderate",
+    groupSize: 4,
+    availability: "Evenings",
+    vibe: "Focused",
+    method: "Practice problems",
+    description: "Looking for 2 more members!",
+    creator: "Alex"
+  },
+  {
+    id: 2,
+    course: "Math 54",
+    pace: "Medium",
+    noiseLevel: "Quiet",
+    groupSize: 3,
+    availability: "Afternoons",
+    vibe: "Chill",
+    method: "Concept discussion",
+    description: "Hoping to study collaboratively and meet new people!",
+    creator: "Jamie"
   }
 ];
