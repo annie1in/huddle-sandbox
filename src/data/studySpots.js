58f5646027a7ee487fa0e86afd3d3b4304d42105
annie1in <annieyoen@gmail.com>
2026-03-11 15:15:51 -0700

    added mock data

---
+++ src/data/studySpots.js
@@ -0,0 +1,44 @@
+export const studySpots = [
+  {
+    id: 1,
+    name: "Doe Library",
+    location: "UC Berkeley",
+    distance: 0.3,
+    noiseLevel: "Silent",
+    seating: "High",
+    outlets: true,
+    lighting: "Bright",
+    crowded: "Low",
+    roomType: "Library",
+    open: true,
+    rating: 4.7
+  },
+  {
+    id: 2,
+    name: "MLK Student Union",
+    location: "UC Berkeley",
+    distance: 0.5,
+    noiseLevel: "Moderate",
+    seating: "Medium",
+    outlets: true,
+    lighting: "Medium",
+    crowded: "High",
+    roomType: "Student Center",
+    open: true,
+    rating: 4.1
+  },
+  {
+    id: 3,
+    name: "Cafe Strada",
+    location: "Berkeley",
+    distance: 0.4,
+    noiseLevel: "Loud",
+    seating: "Medium",
+    outlets: false,
+    lighting: "Dim",
+    crowded: "Medium",
+    roomType: "Cafe",
+    open: true,
+    rating: 4.3
+  }
+];
